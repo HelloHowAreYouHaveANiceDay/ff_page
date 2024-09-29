1b5f2cad93629d122c3018571155c2c2d447a592
HelloHowAreYouHaveANiceDay <liboli.lilibo@gmail.com>
2024-09-28 20:33:13 -0400

    new data files

--- data/raw_fetch.py
+++ data/raw_fetch.py
@@ -6,16 +6,26 @@ PARQUET = 'parquet'
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'raw')
 
+dl_url = "https://github.com/nflverse/nflverse-data/releases/download/"
+
 
 def play_by_play_url(season):
-    url = "https://github.com/nflverse/nflverse-data/releases/download/pbp/"\
-        f"play_by_play_{season}.{PARQUET}"
+    url = dl_url + f"pbp/play_by_play_{season}.{PARQUET}"
+    return url
+
+
+def player_stats_url(season):
+    url = dl_url + f"player_stats/player_stats_{season}.{PARQUET}"
+    return url
+
+
+def snap_count_url(season):
+    url = dl_url + f"snap_counts/snap_counts_{season}.{PARQUET}"
     return url
 
 
 def player_url():
-    url = "https://github.com/nflverse/nflverse-data/releases/download/"\
-        f"players/players.{PARQUET}"
+    url = dl_url + f"players/players.{PARQUET}"
     return url
 
 
@@ -48,22 +58,64 @@ def download_play_by_play(season):
     Parameters:
     season (int): The season for which to download the data.
     """
+    if not os.path.exists(os.path.join(DATA_DIR, 'pbp')):
+        os.makedirs(os.path.join(DATA_DIR, 'pbp'), exist_ok=True)
+
     url = play_by_play_url(season)
     filename = os.path.join(
         DATA_DIR, 'pbp', f'play_by_play_{season}.{PARQUET}')
     download_file(url, filename)
+    return filename
+
+
+def download_snap_counts(season):
+    """
+    Downloads the snap count data for the given season.
+
+    Parameters:
+    season (int): The season for which to download the data.
+    """
+    # create snap_counts folder if doens't exist
+    if not os.path.exists(os.path.join(DATA_DIR, 'snap_counts')):
+        os.makedirs(os.path.join(DATA_DIR, 'snap_counts'), exist_ok=True)
+
+    url = snap_count_url(season)
+    filename = os.path.join(
+        DATA_DIR, 'snap_counts', f'snap_counts_{season}.{PARQUET}')
+    download_file(url, filename)
+    return filename
+
+
+def download_player_stats(season):
+    """
+    Downloads the player stats data for the given season.
+
+    Parameters:
+    season (int): The season for which to download the data.
+    """
+    if not os.path.exists(os.path.join(DATA_DIR, 'player_stats')):
+        os.makedirs(os.path.join(DATA_DIR, 'player_stats'), exist_ok=True)
+
+    url = player_stats_url(season)
+    filename = os.path.join(
+        DATA_DIR, 'player_stats', f'player_stats_{season}.{PARQUET}')
+    download_file(url, filename)
+    return filename
 
 
 def download_players():
     """
     Downloads the player data.
     """
+    if not os.path.exists(os.path.join(DATA_DIR, 'players')):
+        os.makedirs(os.path.join(DATA_DIR, 'players'), exist_ok=True)
+
     url = player_url()
     filename = os.path.join(DATA_DIR, 'players', f'players.{PARQUET}')
     download_file(url, filename)
 
 
-def download_all_seasons():
+def download_pbp_all_seasons():
     """
     Downloads the play-by-play data for all seasons.
     """
